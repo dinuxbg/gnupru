ead8c2e28eb460ffb9f57c2128238cc53568bff8
Dimitar Dimitrov <dinuxbg@gmail.com>
2016-07-29 23:43:57 +0300

    Fix debian source tarball filename
    Signed-off-by: Dimitar Dimitrov <dinuxbg@gmail.com>

--- package-binutils.sh
+++ package-binutils.sh
@@ -16,12 +16,13 @@ RETDIR=`pwd`
 
 [ -d $SRC ] || die $SRC does not exist. Please run ./download-and-patch.sh
 
-VERSION=`head -1 packaging/binutils-pru/debian/changelog | grep -o '(.\+)'| sed -e 's/(\|)//g'`
-cp -Rfp $SRC/binutils-gdb/ packaging/binutils-pru/binutils-pru-$VERSION/ || die Could not copy binutils sources
+VERSION=`head -1 packaging/binutils-pru/debian/changelog | grep -o '(.\+)'| sed -e 's/(\|)//g' | cut -f1 -d-`
+# TODO - remove the last -1 from the version
+cp -Rfp $SRC/binutils-gdb/ packaging/binutils-pru/binutils-pru_$VERSION/ || die Could not copy binutils sources
 cd packaging/binutils-pru || die
 rm -fr binutils-pru*/.git
-tar cjf binutils-pru-$VERSION.orig.tar.bz2 binutils-pru-$VERSION/ || die Could not create binutils source archive
-rm -fr binutils-pru-$VERSION
+tar cjf binutils-pru_$VERSION.orig.tar.bz2 binutils-pru_$VERSION/ || die Could not create binutils source archive
+rm -fr binutils-pru_$VERSION
 
 # TODO: sign the package
 dpkg-buildpackage -us -uc 2>&1 | tee dpkg-buildpackage.log || die Failed to build debian package.
--- package-gcc-newlib.sh
+++ package-gcc-newlib.sh
@@ -16,14 +16,14 @@ RETDIR=`pwd`
 
 [ -d $SRC ] || die $SRC does not exist. Please run ./download-and-patch.sh
 
-VERSION=`head -1 packaging/gcc-pru/debian/changelog | grep -o '(.\+)'| sed -e 's/(\|)//g'`
-cp -Rfp $SRC/gcc/ packaging/gcc-pru/gcc-pru-$VERSION/ || die Could not copy gcc sources
-cp -Rfp $SRC/newlib-cygwin/newlib/ packaging/gcc-pru/gcc-pru-$VERSION/ || die Could not copy newlib sources
-cp -Rfp $SRC/newlib-cygwin/libgloss/ packaging/gcc-pru/gcc-pru-$VERSION/ || die Could not copy newlib sources
+VERSION=`head -1 packaging/gcc-pru/debian/changelog | grep -o '(.\+)'| sed -e 's/(\|)//g' | cut -f1 -d-`
+cp -Rfp $SRC/gcc/ packaging/gcc-pru/gcc-pru_$VERSION/ || die Could not copy gcc sources
+cp -Rfp $SRC/newlib-cygwin/newlib/ packaging/gcc-pru/gcc-pru_$VERSION/ || die Could not copy newlib sources
+cp -Rfp $SRC/newlib-cygwin/libgloss/ packaging/gcc-pru/gcc-pru_$VERSION/ || die Could not copy newlib sources
 cd packaging/gcc-pru || die
 rm -fr gcc-pru*/.git
-tar cjf gcc-pru-$VERSION.orig.tar.bz2 gcc-pru-$VERSION/ || die Could not create gcc source archive
-rm -fr gcc-pru-$VERSION
+tar cjf gcc-pru_$VERSION.orig.tar.bz2 gcc-pru_$VERSION/ || die Could not create gcc source archive
+rm -fr gcc-pru_$VERSION
 
 # TODO: sign the package
 dpkg-buildpackage -us -uc 2>&1 | tee dpkg-buildpackage.log || die Failed to build debian package.
