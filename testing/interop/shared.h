3fb2a7b4462ef999d9e345fef9096f5d86461dd9
Dimitar Dimitrov <dimitar@dinux.eu>
2016-12-26 22:26:54 +0200

    Add TI CLPRU interoperability test
    Signed-off-by: Dimitar Dimitrov <dimitar@dinux.eu>

---
+++ testing/interop/shared.h
@@ -0,0 +1,14 @@
+
+#include <stdint.h>
+
+uint64_t gcc_func_arg1(uint32_t arg1);
+uint32_t gcc_func_arg3(uint32_t arg1, uint32_t arg2, uint32_t arg3);
+
+uint32_t ti_func_arg4(uint32_t n, uint32_t arg1, uint32_t arg2,
+		      uint32_t arg3, uint32_t arg4);
+
+extern uint32_t ti_u32;
+extern uint16_t ti_u16;
+extern uint8_t ti_u8;
+
+
