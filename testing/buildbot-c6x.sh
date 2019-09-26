b838e61d0d1430069dbafe4f2bb2adcffcfc2b32
Dimitar Dimitrov <dimitar@dinux.eu>
2019-09-26 21:27:46 +0300

    Add c6x toolchain build script
    Let's see if it would be useful for the new BB AI.
    
    Signed-off-by: Dimitar Dimitrov <dimitar@dinux.eu>

---
+++ testing/buildbot-c6x.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# Simple script for automatic daily testing of gcc+newlib ToT.
+
+BINUTILS_URL=git://sourceware.org/git/binutils-gdb.git
+GCC_URL=https://github.com/mirrors/gcc
+#GCC_URL=svn://gcc.gnu.org/svn/gcc/trunk
+NEWLIB_URL=https://github.com/mirror/newlib-cygwin
+
+REGRESSION_RECIPIENTS="dinuxbg@gmail.com"
+
+
+bb_daily_target_test()
+{
+  local PREV_BUILD_TAG=${1}
+  local BUILD_TAG=${2}
+
+  bb_clean
+
+  bb_update_source binutils ${BINUTILS_URL}
+  bb_update_source gcc ${GCC_URL}
+  bb_update_source newlib ${NEWLIB_URL}
+
+  # Prepare tree for release, and write proper versioning info.
+  pushd ${WORKSPACE}/gcc || error "failed to enter gcc"
+  ./contrib/gcc_update origin master
+  popd
+
+  local GCC_TOT=`cd gcc && git rev-parse HEAD`
+  local BINUTILS_TOT=`cd binutils && git rev-parse HEAD`
+  local NEWLIB_TOT=`cd newlib && git rev-parse HEAD`
+
+  echo "gcc ${GCC_TOT}" >> ${LOGDIR}/${BUILD_TAG}/versions.txt
+  echo "binutils ${BINUTILS_TOT}" >> ${LOGDIR}/${BUILD_TAG}/versions.txt
+  echo "newlib ${NEWLIB_TOT}" >> ${LOGDIR}/${BUILD_TAG}/versions.txt
+
+  # Build binutils
+  bb_config binutils "--disable-gdb --target=tic6x-none-elf"
+  bb_make binutils "-j`nproc`"
+  bb_make binutils "install"
+
+  export PATH=${PREFIX}/bin:${PATH}
+
+  # GCC pass 1: no libc yet
+  bb_config gcc "--target=tic6x-none-elf --with-newlib --without-headers --enable-languages=c --enable-checking=yes,rtl"
+  bb_make gcc "-j`nproc`"
+  bb_make gcc "install"
+
+  # Libc
+  bb_config newlib "--target=tic6x-none-elf --enable-newlib-io-long-long --enable-newlib-io-long-double --enable-newlib-io-c99-formats"
+  bb_make newlib "-j`nproc`"
+  bb_make newlib "install"
+
+  # GCC pass 2: full feature set
+  bb_config gcc "--target=tic6x-none-elf --with-newlib --enable-languages=c,c++ --enable-checking=yes,rtl"
+  bb_make gcc "-j`nproc`"
+  bb_make gcc "install"
+
+  # Make sure documentation is still in order
+  bb_make gcc "pdf"
+
+  # Save all the logs
+  bb_gather_log_files ${BUILD_TAG}
+}
+
+
+. `dirname ${0}`/buildbot-lib.sh
+
+bb_init ${@}
+
+bb_daily_build
