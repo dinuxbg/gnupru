df76149ce7503a3d81933f76dac7e833edde217f
Dimitar Dimitrov <dimitar@dinux.eu>
2020-12-14 21:11:27 +0200

    testing: Fix AVR Dejagnu setup
    Signed-off-by: Dimitar Dimitrov <dimitar@dinux.eu>

--- testing/buildbot-avr.sh
+++ testing/buildbot-avr.sh
@@ -45,6 +45,17 @@ bb_daily_target_test()
   echo "avr-libc ${AVRLIBC_TOT}" >> ${LOGDIR}/${BUILD_TAG}/versions.txt
   echo "winavr ${WINAVR_URL}" >> ${LOGDIR}/${BUILD_TAG}/versions.txt
 
+  # Setup avrtest, per:
+  #    https://sourceforge.net/p/winavr/code/HEAD/tree/trunk/avrtest/
+  #    https://lists.gnu.org/archive/html/avr-gcc-list/2009-09/msg00016.html
+  export DEJAGNU=${PREFIX}/dejagnurc
+  mkdir -p `dirname ${DEJAGNU}`
+  echo "# WARNING - automatically generated!" > ${DEJAGNU}
+  echo "set avrtest_dir \"${WORKSPACE}/winavr/avrtest\"" >> ${DEJAGNU}
+  echo "set avrlibc_include_dir \"${PREFIX}/avr/include\"" >> ${DEJAGNU}
+  echo 'set boards_dir {}' >> ${DEJAGNU}
+  echo 'lappend boards_dir "${avrtest_dir}/dejagnuboards"' >> ${DEJAGNU}
+
   # Build binutils
   bb_config binutils "--disable-gdb --target=avr"
   bb_make binutils "-j`nproc`"
@@ -65,7 +76,7 @@ bb_daily_target_test()
   # avrtest
   bb_source_command winavr "make -C avrtest"
 
-  # Get the simulator under PATH
+  # Get the simulator under PATH. Needed for gcc test suite.
   export PATH=${WORKSPACE}/winavr/avrtest:${PATH}
 
   # Test binutils. Do not let random test case failures to mark
