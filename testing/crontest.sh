b7303848ade09b7629a057992277e8b1831094a8
Dimitar Dimitrov <dimitar@dinux.eu>
2020-12-15 18:48:34 +0200

    testing: Fix manual AVR test also
    I should refactor those scripts to avoid duplication.
    
    Signed-off-by: Dimitar Dimitrov <dimitar@dinux.eu>

--- testing/crontest.sh
+++ testing/crontest.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
 # Put it in an empty directory and invoke it from crontab. Example:
-#  11 8 * * 7 /home/user/testbot-workspace/crontest-weekly.sh
+#  0 8 * * * /home/user/testbot-workspace/crontest.sh
 
 
 
 cd `dirname ${0}`
 export SUMMARY_RECIPIENTS=dinuxbg@gmail.com
 
-LOGDIR=`pwd`/avr-gcc-8-logs GCC_BRANCH=releases/gcc-8 ./gnupru/testing/buildbot-avr.sh .
-LOGDIR=`pwd`/avr-gcc-8-logs GCC_BRANCH=releases/gcc-8 ./gnupru/testing/buildbot-avr.sh .
-LOGDIR=`pwd`/avr-gcc-9-logs GCC_BRANCH=releases/gcc-9 ./gnupru/testing/buildbot-avr.sh .
-LOGDIR=`pwd`/avr-gcc-10-logs GCC_BRANCH=releases/gcc-10 ./gnupru/testing/buildbot-avr.sh .
-LOGDIR=`pwd`/pru-gcc-10-logs GCC_BRANCH=releases/gcc-10 ./gnupru/testing/buildbot-pru.sh .
-GCC_BRANCH=master ./gnupru/testing/buildbot-avr.sh .
+
+if [ x`date +%u` = x7 ]
+then
+  # Once a week, test AVR and PRU on all active branches.
+  LOGDIR=`pwd`/avr-gcc-8-logs GCC_BRANCH=releases/gcc-8 ./gnupru/testing/buildbot-avr.sh .
+  LOGDIR=`pwd`/avr-gcc-8-logs GCC_BRANCH=releases/gcc-8 ./gnupru/testing/buildbot-avr.sh .
+  LOGDIR=`pwd`/avr-gcc-9-logs GCC_BRANCH=releases/gcc-9 ./gnupru/testing/buildbot-avr.sh .
+  LOGDIR=`pwd`/avr-gcc-10-logs GCC_BRANCH=releases/gcc-10 ./gnupru/testing/buildbot-avr.sh .
+  GCC_BRANCH=master ./gnupru/testing/buildbot-avr.sh .
+
+  LOGDIR=`pwd`/pru-gcc-10-logs GCC_BRANCH=releases/gcc-10 ./gnupru/testing/buildbot-pru.sh .
+fi
+
+# Test PRU everyday.
+GCC_BRANCH=master ./gnupru/testing/buildbot-pru.sh .
--- testing/manual-test-avr.sh
+++ testing/manual-test-avr.sh
@@ -29,6 +29,17 @@ bb_daily_target_test()
   echo "avr-libc ${AVRLIBC_TOT}" >> ${LOGDIR}/${BUILD_TAG}/versions.txt
   echo "winavr ${WINAVR_URL}" >> ${LOGDIR}/${BUILD_TAG}/versions.txt
 
+  # Setup avrtest, per:
+  #    https://sourceforge.net/p/winavr/code/HEAD/tree/trunk/avrtest/
+  #    https://lists.gnu.org/archive/html/avr-gcc-list/2009-09/msg00016.html
+  export DEJAGNU=${PREFIX}/dejagnurc
+  mkdir -p `dirname ${DEJAGNU}`
+  echo "# WARNING - automatically generated!" > ${DEJAGNU}
+  echo "set avrtest_dir \"${WORKSPACE}/winavr/avrtest\"" >> ${DEJAGNU}
+  echo "set avrlibc_include_dir \"${PREFIX}/avr/include\"" >> ${DEJAGNU}
+  echo 'set boards_dir {}' >> ${DEJAGNU}
+  echo 'lappend boards_dir "${avrtest_dir}/dejagnuboards"' >> ${DEJAGNU}
+
   # Build binutils
   bb_config binutils "--disable-gdb --target=avr"
   bb_make binutils "-j`nproc`"
@@ -49,7 +60,7 @@ bb_daily_target_test()
   # avrtest
   bb_source_command winavr "make -C avrtest"
 
-  # Get the simulator under PATH
+  # Get the simulator under PATH. Needed for gcc test suite.
   export PATH=${WORKSPACE}/winavr/avrtest:${PATH}
 
   # Test binutils. Do not let random test case failures to mark
@@ -60,8 +71,8 @@ bb_daily_target_test()
   bb_make --ignore-errors binutils "-k check RUNTESTFLAGS=--target_board=atmega128-sim"
 
   # Test GCC
-  bb_make gcc "check-gcc-c RUNTESTFLAGS=--target_board=atmega128-sim"
-  bb_make gcc "check-gcc-c++ RUNTESTFLAGS=--target_board=atmega128-sim"
+  bb_make gcc "-j`nproc` check-gcc-c RUNTESTFLAGS=--target_board=atmega128-sim"
+  bb_make gcc "-j`nproc` check-gcc-c++ RUNTESTFLAGS=--target_board=atmega128-sim"
 
   # Save all the logs
   bb_gather_log_files ${BUILD_TAG}
