d3d257070222c967a2a299690061dc036d3791d1
Dimitar Dimitrov <dimitar@dinux.eu>
2020-12-20 23:00:06 +0200

    testing: Fix typos
    Signed-off-by: Dimitar Dimitrov <dimitar@dinux.eu>

--- testing/crontest.sh
+++ testing/crontest.sh
@@ -6,13 +6,18 @@
 
 
 cd `dirname ${0}`
+
+# For testing
 export SUMMARY_RECIPIENTS=dinuxbg@gmail.com
+
+# For real.
+# export SUMMARY_RECIPIENTS=gcc-testresults@gcc.gnu.org
+
 
 
 if [ x`date +%u` = x7 ]
 then
   # Once a week, test AVR and PRU on all active branches.
-  LOGDIR=`pwd`/avr-gcc-8-logs GCC_BRANCH=releases/gcc-8 ./gnupru/testing/buildbot-avr.sh .
   LOGDIR=`pwd`/avr-gcc-8-logs GCC_BRANCH=releases/gcc-8 ./gnupru/testing/buildbot-avr.sh .
   LOGDIR=`pwd`/avr-gcc-9-logs GCC_BRANCH=releases/gcc-9 ./gnupru/testing/buildbot-avr.sh .
   LOGDIR=`pwd`/avr-gcc-10-logs GCC_BRANCH=releases/gcc-10 ./gnupru/testing/buildbot-avr.sh .
