4e694e3ec2fb0113a70ed670f5959415c3a0812d
Dimitar Dimitrov <dimitar@dinux.eu>
2021-04-28 18:40:16 +0300

    testing: Add gcc-11 release branch to weekly tests
    Signed-off-by: Dimitar Dimitrov <dimitar@dinux.eu>

--- testing/crontest.sh
+++ testing/crontest.sh
@@ -23,9 +23,11 @@ then
   LOGDIR=`pwd`/avr-gcc-8-logs GCC_BRANCH=releases/gcc-8 ./gnupru/testing/buildbot-avr.sh .
   LOGDIR=`pwd`/avr-gcc-9-logs GCC_BRANCH=releases/gcc-9 ./gnupru/testing/buildbot-avr.sh .
   LOGDIR=`pwd`/avr-gcc-10-logs GCC_BRANCH=releases/gcc-10 ./gnupru/testing/buildbot-avr.sh .
+  LOGDIR=`pwd`/avr-gcc-11-logs GCC_BRANCH=releases/gcc-11 ./gnupru/testing/buildbot-avr.sh .
   GCC_BRANCH=master ./gnupru/testing/buildbot-avr.sh .
 
   LOGDIR=`pwd`/pru-gcc-10-logs GCC_BRANCH=releases/gcc-10 ./gnupru/testing/buildbot-pru.sh .
+  LOGDIR=`pwd`/pru-gcc-11-logs GCC_BRANCH=releases/gcc-11 ./gnupru/testing/buildbot-pru.sh .
 fi
 
 # Test PRU everyday.
