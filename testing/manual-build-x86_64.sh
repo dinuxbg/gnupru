374ba47fbbda9e4b99d88c3fdcc176831086e0ea
Dimitar Dimitrov <dimitar@dinux.eu>
2021-05-08 20:50:24 +0300

    testing: Add more targets for cross-checking issues
    Signed-off-by: Dimitar Dimitrov <dimitar@dinux.eu>

---
+++ testing/manual-build-x86_64.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# Simple script for manual building of gcc+glibc ToT.
+#
+# It is assumed that source directories are already setup, and
+# desired HEADs are checked out.
+
+BB_ARCH=x86_64
+
+BTARGET=x86_64-pc-linux-gnu
+
+# Do not send any email for this session
+Mail()
+{
+  true
+}
+
+bb_daily_target_test()
+{
+  local PREV_BUILD_TAG=${1}
+  local BUILD_TAG=${2}
+
+  bb_clean
+
+  local GCC_TOT=`cd gcc && git rev-parse HEAD`
+  local BINUTILS_TOT=`cd binutils && git rev-parse HEAD`
+  local GLIBCL_TOT=`cd glibc && git rev-parse HEAD`
+
+  echo "gcc ${GCC_TOT}" >> ${LOGDIR}/${BUILD_TAG}/versions.txt
+  echo "binutils ${BINUTILS_TOT}" >> ${LOGDIR}/${BUILD_TAG}/versions.txt
+  echo "glibc ${GLIBC_TOT}" >> ${LOGDIR}/${BUILD_TAG}/versions.txt
+
+  # Build binutils
+  bb_config binutils "--disable-gdb --target=${BTARGET}"
+  bb_make binutils "-j`nproc`"
+  bb_make binutils "install"
+
+  export PATH=${PREFIX}/bin:${PATH}
+
+  # GCC pass 1: no libc yet
+  bb_config gcc "--target=${BTARGET} --disable-multilib --without-headers --enable-languages=c --enable-checking=yes,rtl --disable-libssp --disable-gcov --with-sysroot=/"
+  bb_make gcc "-j`nproc`"
+  bb_make gcc "install"
+
+  # Libc
+  bb_config glibc "--target=${BTARGET} --disable-multilib"
+  bb_make glibc "-j`nproc`"
+  bb_make glibc "install"
+
+  # libiberty complains about changed LDFLAGS.
+  bb_make gcc "distclean"
+
+  # GCC pass 2: full feature set
+  bb_config gcc "--target=${BTARGET} --disable-multilib --enable-languages=c,c++ --enable-checking=yes,rtl --disable-libssp --disable-gcov"
+  bb_make gcc "-j`nproc`"
+  bb_make gcc "install"
+
+  # Make sure documentation is still in order
+  # bb_make gcc "pdf"
+
+  # Save all the logs
+  bb_gather_log_files ${BUILD_TAG}
+}
+
+
+. `dirname ${0}`/buildbot-lib.sh
+
+bb_init ${@}
+
+bb_daily_build
