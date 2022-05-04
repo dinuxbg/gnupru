780c133df2bfa8332725eac4cbeac4fefc9d8223
Dimitar Dimitrov <dimitar@dinux.eu>
2022-05-04 22:18:04 +0300

    testing: Add GCC 12 branch to biweekly tests
    Signed-off-by: Dimitar Dimitrov <dimitar@dinux.eu>

--- testing/crontest.sh
+++ testing/crontest.sh
@@ -16,17 +16,19 @@ export SUMMARY_RECIPIENTS=dinuxbg@gmail.com
 # export SUMMARY_RECIPIENTS=gcc-testresults@gcc.gnu.org
 
 
-
-if [ x`date +%u` = x7 ]
+# On every second Sunday...
+if [ x`date +%u` = x7 -a x$((`date +%U` % 2)) = x0 ]
 then
-  # Once a week, test AVR and PRU on all active branches.
+  # Test AVR and PRU on all active branches.
   LOGDIR=`pwd`/avr-gcc-9-logs GCC_BRANCH=releases/gcc-9 ./gnupru/testing/buildbot-avr.sh .
   LOGDIR=`pwd`/avr-gcc-10-logs GCC_BRANCH=releases/gcc-10 ./gnupru/testing/buildbot-avr.sh .
   LOGDIR=`pwd`/avr-gcc-11-logs GCC_BRANCH=releases/gcc-11 ./gnupru/testing/buildbot-avr.sh .
+  LOGDIR=`pwd`/avr-gcc-12-logs GCC_BRANCH=releases/gcc-12 ./gnupru/testing/buildbot-avr.sh .
   GCC_BRANCH=master ./gnupru/testing/buildbot-avr.sh .
 
   LOGDIR=`pwd`/pru-gcc-10-logs GCC_BRANCH=releases/gcc-10 ./gnupru/testing/buildbot-pru.sh .
   LOGDIR=`pwd`/pru-gcc-11-logs GCC_BRANCH=releases/gcc-11 ./gnupru/testing/buildbot-pru.sh .
+  LOGDIR=`pwd`/pru-gcc-12-logs GCC_BRANCH=releases/gcc-12 ./gnupru/testing/buildbot-pru.sh .
 fi
 
 # Test PRU everyday.
