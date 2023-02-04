69266b19ec7e85a4085044c0af95c931af6d8e07
Dimitar Dimitrov <dimitar@dinux.eu>
2023-02-04 16:04:33 +0200

    Bump to latest unreleased upstream
    This should fix issue #46.
    
    Signed-off-by: Dimitar Dimitrov <dimitar@dinux.eu>

--- download-and-prepare.sh
+++ download-and-prepare.sh
@@ -3,10 +3,17 @@
 # Sample script to download vanilla upstream projects.
 
 # Official packages to download.
-GCC_URL=http://ftpmirror.gnu.org/gcc/gcc-12.1.0/gcc-12.1.0.tar.xz
-BINUTILS_URL=http://ftpmirror.gnu.org/binutils/binutils-2.38.tar.bz2
+
+# TODO - wait for GCC 13 release!  
+#GCC_URL=http://ftpmirror.gnu.org/gcc/gcc-13.0/gcc-13.0.tar.xz
+GCC_URL=https://gcc.gnu.org/pub/gcc/snapshots/13-20230129/gcc-13-20230129.tar.xz
+
+# TODO - wait for 2.41 release!
+#BINUTILS_URL=http://ftpmirror.gnu.org/binutils/binutils-2.41.tar.bz2
+BINUTILS_URL=https://github.com/bminor/binutils-gdb/archive/ba268471bf9a2b76efc0d7a9c05291f7e8cd4424.tar.gz
+
 NEWLIB_URL=ftp://sourceware.org/pub/newlib/newlib-4.2.0.20211231.tar.gz
-GNUPRUMCU_URL=https://github.com/dinuxbg/gnuprumcu/releases/download/v0.9.0/gnuprumcu-0.9.0.tar.gz
+GNUPRUMCU_URL=https://github.com/dinuxbg/gnuprumcu/releases/download/v0.9.1/gnuprumcu-0.9.1.tar.gz
 
 MAINDIR=`pwd`
 SRC=`pwd`/src
