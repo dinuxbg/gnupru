dc03e8142b85f062c8be1510b20db9af69125a1b
Dimitar Dimitrov <dimitar@dinux.eu>
2023-04-17 22:03:27 +0300

    testing: Start testing the gcc-13 release branch
    Signed-off-by: Dimitar Dimitrov <dimitar@dinux.eu>

--- testing/crontest.sh
+++ testing/crontest.sh
@@ -23,11 +23,13 @@ then
   LOGDIR=`pwd`/avr-gcc-10-logs GCC_BRANCH=releases/gcc-10 ./gnupru/testing/buildbot-avr.sh .
   LOGDIR=`pwd`/avr-gcc-11-logs GCC_BRANCH=releases/gcc-11 ./gnupru/testing/buildbot-avr.sh .
   LOGDIR=`pwd`/avr-gcc-12-logs GCC_BRANCH=releases/gcc-12 ./gnupru/testing/buildbot-avr.sh .
+  LOGDIR=`pwd`/avr-gcc-13-logs GCC_BRANCH=releases/gcc-13 ./gnupru/testing/buildbot-avr.sh .
   GCC_BRANCH=master ./gnupru/testing/buildbot-avr.sh .
 
   LOGDIR=`pwd`/pru-gcc-10-logs GCC_BRANCH=releases/gcc-10 ./gnupru/testing/buildbot-pru.sh .
   LOGDIR=`pwd`/pru-gcc-11-logs GCC_BRANCH=releases/gcc-11 ./gnupru/testing/buildbot-pru.sh .
   LOGDIR=`pwd`/pru-gcc-12-logs GCC_BRANCH=releases/gcc-12 ./gnupru/testing/buildbot-pru.sh .
+  LOGDIR=`pwd`/pru-gcc-13-logs GCC_BRANCH=releases/gcc-13 ./gnupru/testing/buildbot-pru.sh .
 fi
 
 # Test PRU everyday.
