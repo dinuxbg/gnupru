4afc38cbe958293459bc9a37a11bba131e9f5b7a
Dimitar Dimitrov <dimitar@dinux.eu>
2024-05-22 21:28:26 +0300

    Use HTTP for downloading newlib
    Signed-off-by: Dimitar Dimitrov <dimitar@dinux.eu>

--- download-and-prepare.sh
+++ download-and-prepare.sh
@@ -5,7 +5,7 @@
 # Official packages to download.
 GCC_URL=http://ftpmirror.gnu.org/gcc/gcc-14.1.0/gcc-14.1.0.tar.xz
 BINUTILS_URL=http://ftpmirror.gnu.org/binutils/binutils-2.42.tar.bz2
-NEWLIB_URL=ftp://sourceware.org/pub/newlib/newlib-4.4.0.20231231.tar.gz
+NEWLIB_URL=http://sourceware.org/pub/newlib/newlib-4.4.0.20231231.tar.gz
 GNUPRUMCU_URL=https://github.com/dinuxbg/gnuprumcu/releases/download/v0.9.5/gnuprumcu-0.9.5.tar.gz
 
 MAINDIR=`pwd`
